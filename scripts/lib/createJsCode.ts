8865eefb07f6f7913891cd58d76d0598815bf97d
nawoo <now.and.nawoo@gmail.com>
2022-12-21 22:27:40 +0900

    コメント追加

--- scripts/lib/createJsCode.ts
+++ scripts/lib/createJsCode.ts
@@ -4,10 +4,10 @@ import UglifyJS from 'uglify-js';
 import { join, parse } from 'path';
 
 export function createJsCode(htmlPath: string, outputDir: string): void {
-  const fileContent = readFileSync(htmlPath, 'utf-8');
-  const rawJsCode = between(fileContent, '// --- start ---', '// --- end ---');
-  const minifiedJsCode = UglifyJS.minify(rawJsCode).code;
-  const jsCode = encodeURIComponent(encodeURIComponent(minifiedJsCode)); // 埋め込み用JsCode
+  const fileContent = readFileSync(htmlPath, 'utf-8'); // HTMLファイルを読み込む
+  const rawJsCode = between(fileContent, '// --- start ---', '// --- end ---'); // JavaScriptコードを切り出す
+  const minifiedJsCode = UglifyJS.minify(rawJsCode).code; // Minify
+  const jsCode = encodeURIComponent(encodeURIComponent(minifiedJsCode)); // URLエンコードを2回
   console.log('jsCode.length', rawJsCode.length, minifiedJsCode.length, jsCode.length);
   const { name } = parse(htmlPath);
   writeFileSync(join(outputDir, name + '.jscode'), jsCode);
